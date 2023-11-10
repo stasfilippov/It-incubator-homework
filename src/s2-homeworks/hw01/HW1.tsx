3fe7c98e7148b787c4ee235441859823b284a521
stasfilippov <kron888999@gmail.com>
2023-11-11 00:14:51 +0300

    -add complete hw-02-01

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -9,7 +9,7 @@ import avatar from './avatar.png'
 * 1 - описать тип MessageType +++
 * 2 - описать тип MessagePropsType в файле Message.tsx +++
 * 3 - в файле Message.tsx отобразить приходящие данные +++
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
+* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx +++
 * 5 - сделать стили в соответствии с дизайном
 * */
 export type MessageUserType = {
@@ -33,10 +33,10 @@ export const message0: MessageType = {
 	id: 0,
 	user: {
 		avatar: avatar, // можно менять
-		name: 'Some Name',  // можно менять
+		name: 'Ivan',  // можно менять
 	},
 	message: {
-		text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+		text: 'Hello, she didn’t do anything and rested all day, how are you?', // можно менять
 		time: '22:00', // можно менять
 	},
 }
