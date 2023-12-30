de4a9712453ae9457763639b348b48cb75c3cc4a
stasfilippov <kron888999@gmail.com>
2023-12-30 22:23:15 +0300

    -complete hw02-04

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -5,20 +5,20 @@ import Stand from './Stand'
 /*
 * 1 - понять (и простить) SuperInputText
 * 2 - в зависимости от типа и дизэйбла прицепить нужный класс в SuperButton.tsx (строка 21)
-* 3 - дописать onChangeCallback в SuperCheckbox.tsx чтоб оба чекбокса работали на стенде
-* 4 - сделать стили в соответствии с дизайном
-* */
+! 3 - дописать onChangeCallback в SuperCheckbox.tsx чтоб оба чекбокса работали на стенде
+! 4 - сделать стили в соответствии с дизайном
+! */
 
 const HW4 = () => {
-    return (
-        <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
-            {/*демонстрация возможностей компонент:*/}
-            <div className={s2.hw}>
-                <Stand />
-            </div>
-        </div>
-    )
+	return (
+		<div id={'hw4'}>
+			<div className={s2.hwTitle}>Homework #4</div>
+			{/*демонстрация возможностей компонент:*/}
+			<div className={s2.hw}>
+				<Stand/>
+			</div>
+		</div>
+	)
 }
 
 export default HW4
