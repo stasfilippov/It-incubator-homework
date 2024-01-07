14c640a87a4bdb179c81dac0503b180e55e818ff
stasfilippov <kron888999@gmail.com>
2024-01-07 21:27:07 +0300

    -complete hw03-01

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
-import { Layout } from './layout/Layout'
+import {HashRouter} from 'react-router-dom'
+import {Layout} from './layout/Layout'
 import Pages from './Pages'
 
 /*
@@ -10,14 +10,14 @@ import Pages from './Pages'
 * */
 
 function HW5() {
-    return (
-        <HashRouter>
-            {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
-            <Layout>
-                <Pages />
-            </Layout>
-        </HashRouter>
-    )
+	return (
+		<HashRouter>
+			{/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
+			<Layout>
+				<Pages/>
+			</Layout>
+		</HashRouter>
+	)
 }
 
 export default HW5
