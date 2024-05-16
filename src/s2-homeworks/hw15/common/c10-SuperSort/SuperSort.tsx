549e0b90a5ddf26a69c1a04e8fb0f5a63011ae3c
stasfilippov <kron888999@gmail.com>
2024-05-16 18:40:18 +0300

    -fix superSort, superPagination,

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,65 @@
 import React from 'react'
+import downIcon from '../../assets/arrowDown.png'
+import upIcon from '../../assets/arrowUp.png'
 
-// добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const noneIcon =
+	'https://cdn.icon-icons.com/icons2/906/PNG/512/sort_icon-icons.com_69899.png'
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
+	id?: string
+	sort: string
+	value: string
+	onChange: (newSort: string) => void
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+	switch (sort) {
+		case '':
+			return down
+		case down:
+			return up
+		case up:
+			return ''
+		default:
+			return down
+	}
 }
 
-const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
-) => {
-    const up = '0' + value
-    const down = '1' + value
+const SuperSort: React.FC<SuperSortPropsType> = ({
+	sort,
+	value,
+	onChange,
+	id = 'hw15',
+}) => {
+	const up = '0' + value
+	const down = '1' + value
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+	const onChangeCallback = () => {
+		onChange(pureChange(sort, down, up))
+	}
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+	const icon = sort === down ? downIcon : sort === up ? upIcon : noneIcon
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
-
-            {icon} {/*а это убрать*/}
-        </span>
-    )
+	return (
+		<span id={id + '-sort-' + value} onClick={onChangeCallback}>
+			<div
+				style={{
+					width: '20px',
+					height: '20px',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<img
+					style={{ width: '100%', height: '100%' }}
+					src={icon}
+					alt='icon'
+					id={id + '-icon-' + sort}
+				/>
+			</div>
+		</span>
+	)
 }
 
 export default SuperSort
--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -18,7 +18,7 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = ({
 	onChange,
 	id = 'hw15',
 }) => {
-	const lastPage = totalCount / itemsCountForPage //* пишет студент
+	const lastPage = Math.floor(totalCount / itemsCountForPage) //* пишет студент
 
 	const onChangeCallback = (
 		event: React.ChangeEvent<unknown>,
@@ -28,9 +28,7 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = ({
 	}
 
 	const onChangeSelect = (event: any) => {
-		console.log(event)
-
-		// onChange(page, event.currentTarget.value) // пишет студент
+		onChange(page, event) // *пишет студент
 	}
 
 	return (
@@ -42,6 +40,9 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = ({
 						// стили для Pagination // пишет студент
 					}
 				}
+				size='small'
+				shape='rounded'
+				color='primary'
 				page={page}
 				count={lastPage}
 				onChange={onChangeCallback}
@@ -49,7 +50,7 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = ({
 				hidePrevButton
 			/>
 
-			<span className={s.text1}>показать</span>
+			<span className={s.text1}>Показать</span>
 
 			<SuperSelect
 				id={id + '-pagination-select'}
@@ -60,9 +61,10 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = ({
 					{ id: 10, value: 10 },
 				]}
 				onChange={onChangeSelect}
+				className={s.select}
 			/>
 
-			<span className={s.text2}>строк в таблице</span>
+			<span className={s.text2}>строки в таблице</span>
 		</div>
 	)
 }
